a72eff758a54817ffa17753d0b1077e63c506e77
a2k-amit <amitkarlekar.sfdc@gmail.com>
2024-08-09 16:29:18 +0530

    changes made to captcha

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+let captchaChecked = false;
+function isCaptchaTrue() {
+  captchaChecked = true;
+}
 function handlebeforesubmit(event) {
-  let outputleaddate = document.querySelector(".output_lead_date");
-  let inputleaddate = document.querySelector(".input_lead_date");
-  console.log("input lead date value -", inputleaddate.value);
+  if (captchaChecked) {
+    let outputleaddate = document.querySelector(".output_lead_date");
+    let inputleaddate = document.querySelector(".input_lead_date");
+    console.log("input lead date value -", inputleaddate.value);
 
-  let formatDate = new Date(inputleaddate.value).toLocaleDateString("en-IN");
+    let formatDate = new Date(inputleaddate.value).toLocaleDateString("en-IN");
 
-  outputleaddate.value = formatDate;
-  console.log("ouput lead date value -", outputleaddate.value);
+    outputleaddate.value = formatDate;
+    console.log("ouput lead date value -", outputleaddate.value);
+  } else {
+    alert("Please verify the captcha");
+    event.preventDefault();
+  }
 }
 
 function timestamp() {
