314621f39c111df6340e0b9f5f1cba4e48463d4d
a2k-amit <amitkarlekar.sfdc@gmail.com>
2024-08-09 15:28:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function handlebeforesubmit(event) {
+  let outputleaddate = document.querySelector(".output_lead_date");
+  let inputleaddate = document.querySelector(".input_lead_date");
+  console.log("input lead date value -", inputleaddate.value);
+
+  let formatDate = new Date(inputleaddate.value).toLocaleDateString("en-IN");
+
+  outputleaddate.value = formatDate;
+  console.log("ouput lead date value -", outputleaddate.value);
+}
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
