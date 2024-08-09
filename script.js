679e0fef5d6146cb6e6c1a5642255b7dc401b10a
a2k-amit <amitkarlekar.sfdc@gmail.com>
2024-08-09 18:34:59 +0530

    javascript aligned

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ let captchaChecked = false;
 function isCaptchaTrue() {
   captchaChecked = true;
 }
+
 function handlebeforesubmit(event) {
   if (captchaChecked) {
     let outputleaddate = document.querySelector(".output_lead_date");
     let inputleaddate = document.querySelector(".input_lead_date");
-    console.log("input lead date value -", inputleaddate.value);
 
-    let formatDate = new Date(inputleaddate.value).toLocaleDateString("en-IN");
+    console.log("input_lead_date.value", inputleaddate.value); // string >> date(en_IN)
 
-    outputleaddate.value = formatDate;
-    console.log("ouput lead date value -", outputleaddate.value);
+    let formatedDate = new Date(inputleaddate.value).toLocaleDateString(
+      "en-IN"
+    );
+    outputleaddate.value = formatedDate;
   } else {
-    alert("Please verify the captcha");
+    alert("Please Check The Captcha");
     event.preventDefault();
   }
 }
